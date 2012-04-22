f3b8f0d7cc19e1ba66fdff541a99855a44646c9d
Ross Beehler <ross@sipp.net>
2012-04-22 11:19:49 -0400

    finished two pair spec

--- spec/poker_hands/two_pair_poker_hand_spec.rb
+++ spec/poker_hands/two_pair_poker_hand_spec.rb
@@ -20,26 +20,8 @@ describe TwoPairPokerHand do
         hand_of(%W(2S 2C 3S 3C KS)).should_be_less_than(%W(2H 2D 3H 3D AH))
     end
 
-    it "declares draw if all cards match"
-#      hand_of(%W(JS JC 2S 3S 4S)).should_be_greater_than(%W(10H 10D 2H 3H AS))
-#    end
-#
-#    context "with matching pairs" do
-#      it "uses highest non-pair card" do
-#        hand_of(%W(4S 4C 2S 3S AS)).should_be_greater_than(%W(4H 4D 2H 3H KS))
-#      end
-#
-#      it "uses 2nd highest non-pair card if highest non-pair cards match" do
-#        hand_of(%W(4S 4C 2S KS AS)).should_be_greater_than(%W(4H 4D 2H QH AS))
-#      end
-#
-#      it "uses 3rd highest non-pair card if 1st and 2nd non-pair cards match" do
-#        hand_of(%W(4S 4C QS KS AS)).should_be_greater_than(%W(4H 4D JH KH AS))
-#      end
-#
-#      it "declares draw if all cards match" do
-#        hand_of(%W(2S 2C 3S 4S 5S)).should_equal(%W(2H 2D 3H 4H 5H))
-#      end
-#    end
+    it "declares draw if all cards match" do
+        hand_of(%W(2S 2C 3S 3C AS)).should_equal(%W(2H 2D 3H 3D AH))
+    end
   end
 end
